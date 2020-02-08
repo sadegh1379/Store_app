caa598df21f6a0ffbb0316c89c5fc35896735b72
Sadegh Akbari <akbarisadegh382@gmail.com>
2020-02-08 17:28:40 +0400

    cart updated

--- src/component/buy/GetBuy.js
+++ src/component/buy/GetBuy.js
@@ -97,11 +97,14 @@ class  GetBuy extends Component{
                 <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
                     <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                        <h5 className="modal-title" id="exampleModalLongTitle">Your Cart </h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
+
+                    {/* modal body */}
+
                     <div className="modal-body">
                         ...
                     </div>
